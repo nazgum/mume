80a888b378b09a3b1bb65205a461d97f50968dde
Nazgum <work@nazgum.com>
2024-01-29 13:51:20 -0500

    2024 updates + blight starting setup

--- scripts/who-get.rb
+++
@@ -1,4 +0,0 @@
-require 'open-uri'
-
-count = URI.open("https://nazgum.com/mume/player-count.txt").read.to_i
-File.open("/home/nazgum/mume/scripts/player-count.txt", "w") {|f| f.write(count) }
--- scripts/who.rb
+++
@@ -1,2 +0,0 @@
-count = File.open("/home/nazgum/mume/scripts/player-count.txt") {|f| f.read }
-puts "#var {players} {#{count}}"
